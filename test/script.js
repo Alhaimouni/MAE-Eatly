33710fa2b2765b3d41e2672347630bad9c9a4ab8
Mohammad Al-Haimouni <m.alhaimouni@gmail.com>
2023-07-24 00:09:20 +0300

    sec6

--- test/script.js
+++ test/script.js
@@ -1,26 +1,12 @@
-"use strict";
-
 let currentCardIndex = 0;
-const totalCards = document.querySelectorAll(".card").length;
+const totalCards = 3;
 
 function showCard(index) {
   const cards = document.querySelectorAll(".card");
-  const [back, next] = document.querySelectorAll(".slider button");
   if (index >= 0 && index < totalCards) {
     cards.forEach((card) => (card.style.display = "none"));
     cards[index].style.display = "block";
     currentCardIndex = index;
-    
-    if (index == 0) {
-      back.style.opacity = "0.2";
-    } else {
-      back.style.opacity = "1";
-    }
-    if (index == totalCards - 1) {
-      next.style.opacity = "0.2";
-    } else {
-      next.style.opacity = "1";
-    }
   }
 }
 
