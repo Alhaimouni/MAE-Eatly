e073fd61b71793d1ba881daf3053d094330eea32
Mohammad Al-Haimouni <m.alhaimouni@gmail.com>
2023-07-25 16:53:37 +0300

    section 6

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 "use strict";
-
+const { log } = console;
 let currentCardIndex = 0;
 const totalCards = document.querySelectorAll(".card").length;
 
@@ -10,16 +10,19 @@ function showCard(index) {
     cards.forEach((card) => (card.style.display = "none"));
     cards[index].style.display = "block";
     currentCardIndex = index;
-    
     if (index == 0) {
       back.style.opacity = "0.2";
+      back.disabled = true;
     } else {
       back.style.opacity = "1";
+      back.disabled = false;
     }
     if (index == totalCards - 1) {
       next.style.opacity = "0.2";
+      next.disabled = true;
     } else {
-      next.style.opacity = "1";
+      next.style.opacity = "1.0";
+      next.disabled = false;
     }
   }
 }
@@ -30,3 +33,36 @@ function swapCards(offset) {
 }
 
 showCard(currentCardIndex);
+
+//============================================================================================
+
+//section five slider
+
+const [back5, next5] = document.querySelectorAll(".slide-5 .btns button");
+const pds = document.querySelectorAll(".slide-5 .pd");
+
+let head = 0;
+
+next5.addEventListener("click", () => {
+  if (head > pds.length - 3) {
+    return;
+  }
+  pds[head].style.display = "none";
+  head++;
+});
+
+back5.addEventListener("click", () => {
+  if (head < 1) {
+    for (let i = 0; i < pds.length; i++) {
+      pds[i].style.display = "inline-block";
+    }
+    head = 0;
+    return;
+  }
+  for (let i = 0; i < pds.length; i++) {
+    pds[i].style.display = "none";
+  }
+  pds[head - 1].style.display = "inline-block";
+  pds[head].style.display = "inline-block";
+  head--;
+});
